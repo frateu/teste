64c1a6bc59a0d1a27a5dd7ea6aeb2d07edc3342b
Matheus Freitas <teusanjos0@gmail.com>
2021-02-11 12:16:46 -0300

    Nova Parte

---
+++ Q2Vetores.py
@@ -0,0 +1,21 @@
+vetor = []
+i = 0
+sumPar = 0
+sumImp = 0
+
+while i < 7:
+
+    vetor.append(int(input("Insira o numero de posição " + str(i) + ": ")))
+    i += 1
+
+print("\n\nO vetor ficou organizado da seguinte forma: " + str(vetor))
+
+i = 0
+while i < 7:
+    if vetor[i] % 2 == 0:
+        sumPar += vetor[i]
+    else:
+        sumImp += vetor[i]
+    i += 1
+
+print("\n\nSoma Par: " + str(sumPar) + "\nSoma Impar: " + str(sumImp))
